a0f7752ac127f6d507fa48a60f5beadb67714ceb
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2024-02-13 12:45:26 +0530

    Scanner class

---
+++ Java/scanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        float f = s.nextFloat();
+        String str = s.nextLine();
+        //char c = s.charAt(index);
+       // char ch = s.next();
+    }
+}
