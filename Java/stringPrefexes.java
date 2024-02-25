dc097cb9666191bef51c33716f180f0d435267d1
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2024-02-25 17:05:19 +0530

    25-02-2024

--- Java/stringPrefexes.java
+++ Java/stringPrefexes.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class stringPrefexes {
     public static void printPrefix(String str){
-
+for(int i=0;i<str.length();i++){
+    System.out.println(str.substring(0, i+1));
+}
  
 
 
@@ -10,6 +14,10 @@ public class stringPrefexes {
 
     }
     public static void main(String[] args) {
+        String s = new String();
+        Scanner sc = new Scanner(System.in);
+        s = sc.next();
+        printPrefix(s);
         
     }
 }
